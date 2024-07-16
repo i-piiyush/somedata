b2b4dafe75d77fc11dd643ddbc7a17daa27679bc
i-piiyush <148619340+i-piiyush@users.noreply.github.com>
2024-07-16 16:47:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,310 @@
+let game = [
+  {
+    gameName: "Clash of Clans",
+    gameImage: `<img src="https://wallpapercave.com/wp/wc1808412.jpg" >`,
+    gameId: "gameId",
+    character: [
+      {
+        charName: "Barbarian",
+        charDesc: "A fierce warrior with a huge mustache",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashofclans/images/4/4e/Barbarian_info.png/revision/latest/scale-to-width-down/250?cb=20170927230429" alt="">`,
+
+        charBgColor: "#FFD700", // gold
+        charTextColor: "#000000",
+        charId: "charID", // black
+      },
+      {
+        charName: "Archer",
+        charDesc: "A long-ranged attacker with deadly precision",
+        charImg: `<img src="./Archer_info.webp" alt="">`,
+        charBgColor: "#FF69B4", // pink
+        charTextColor: "#FFFFFF",
+        charId: "charID", // white
+      },
+      {
+        charName: "Giant",
+        charDesc: "A massive unit with immense strength",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashofclans/images/3/34/Archer_info.png/revision/latest/scale-to-width-down/250?cb=20170927230509" alt="">`,
+        charBgColor: "#808080", // gray
+        charTextColor: "#FFFFFF",
+        charId: "charID", // white
+      },
+      {
+        charName: "Goblin",
+        charDesc: "A fast-moving looter with a keen eye for treasure",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashofclans/images/f/fa/Goblin_info.png/revision/latest/scale-to-width-down/284?cb=20170927232755" alt="">`,
+        charBgColor: "#008000", // green
+        charTextColor: "#FFFFFF", // white
+        charId: "charID",
+      },
+      {
+        charName: "Wizard",
+        charDesc: "A magical unit that deals high damage",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashofclans/images/1/14/Wizard_info.png/revision/latest/scale-to-width-down/250?cb=20170927230801" alt="">`,
+        charBgColor: "#0000FF", // blue
+        charTextColor: "#FFFFFF", // white
+        charId: "charID",
+      },
+      {
+        charImg: "Balloon",
+        charDesc: "An aerial unit that drops bombs on enemies",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashofclans/images/2/2f/Balloon_info.png/revision/latest/scale-to-width-down/220?cb=20170927230730" alt="">`,
+        charBgColor: "#DC143C", // crimson
+        charTextColor: "#FFFFFF",
+        charId: "charID", // white
+      },
+      {
+        charName: "P.E.K.K.A",
+        charDesc: "A heavily armored and powerful unit",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashofclans/images/5/54/P.E.K.K.A_info.png/revision/latest/scale-to-width-down/250?cb=20170927230947" alt="">`,
+        charBgColor: "#800080", // purple
+        charTextColor: "#FFFFFF",
+        charId: "charID", // white
+      },
+    ],
+  },
+  {
+    gameName: "Clash Royale",
+    gameImage: `<img src="https://wallpapers.com/images/high/clash-royale-happy-and-angry-king-67pap6bmkrprchab.webp" >`,
+    gameId: "gameId",
+    character: [
+      {
+        charName: "Knight",
+        charDesc: "A tough melee fighter",
+        charImg: `<img src="https://www.clasher.us/images/cr/units/KnightCard.png">`,
+        charBgColor: "#A52A2A", // brown
+        charTextColor: "#FFFFFF", // white
+        charId: "charID",
+      },
+      {
+        charName: "Prince",
+        charDesc: "A fast-moving, single-target attacker",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashroyale/images/b/be/PrinceCard.png/revision/latest?cb=20171212203631" alt="">`,
+        charBgColor: "#FFFF00", // yellow
+        charTextColor: "#000000", // black
+        charId: "charID",
+      },
+      {
+        charName: "Baby Dragon",
+        charDesc: "A flying unit that deals splash damage",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashroyale/images/3/35/BabyDragonCard.png/revision/latest?cb=20171212203300" alt="">`,
+        charBgColor: "#32CD32", // lime green
+        charTextColor: "#FFFFFF", // white
+        charId: "charID",
+      },
+      {
+        charName: "Skeleton Army",
+        charDesc: "A swarm of skeletons that overwhelms enemies",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashroyale/images/d/d0/SkeletonArmyCard.png/revision/latest?cb=20171212203647" alt="">`,
+        charBgColor: "#D3D3D3", // light gray
+        charTextColor: "#000000", // black
+        charId: "charID",
+      },
+      {
+        charName: "Mini P.E.K.K.A",
+        charDesc: "A single-target melee unit with high damage",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashroyale/images/7/7b/MiniPEKKACard.png/revision/latest?cb=20220702163933" alt="">`,
+        charBgColor: "#4682B4", // steel blue
+        charTextColor: "#FFFFFF", // white
+        charId: "charID",
+      },
+      {
+        charName: "Hog Rider",
+        charDesc: "A fast-moving, high-damage unit that targets buildings",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashroyale/images/3/30/HogRiderCard.png/revision/latest?cb=20171212204811" alt="">`,
+        charBgColor: "#8B4513", // saddle brown
+        charTextColor: "#FFFFFF", // white
+        charId: "charID",
+      },
+      {
+        charName: "Witch",
+        charDesc: "A magical unit that summons skeletons",
+        charImg: `<img src="https://static.wikia.nocookie.net/clashroyale/images/7/7f/WitchCard.png/revision/latest?cb=20220519204921" alt="">`,
+        charBgColor: "#4B0082", // indigo
+        charTextColor: "#FFFFFF", // white
+        charId: "charID",
+      },
+    ],
+  },
+  {
+    gameName: "Brawl Stars",
+    gameImage: `<img src="https://wallpapers.com/images/featured-full/brawl-stars-gkfzjpvuf1h0ypyi.jpg" >`,
+    gameId: "gameId",
+    characters: [
+      {
+        charName: "Shelly",
+        charDesc: "A shotgun-wielding brawler with wide spread attacks",
+        charImg: `<img src="https://static.wikia.nocookie.net/brawlstars/images/5/5e/Shelly_Skin-Default.png/revision/latest?cb=20240301164742" alt="">`,
+        charBgColor: "#8A2BE2", // blue violet
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Colt",
+        charDesc: "A dual-wielding shooter with rapid fire rate",
+        charImg: `<img src="https://static.wikia.nocookie.net/brawlstars/images/8/8a/Colt_Skin-Default.png/revision/latest?cb=20231111120541" alt="">`,
+        charBgColor: "#FF4500", // orange red
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Nita",
+        charDesc: "A brawler who can summon a bear to assist in battle",
+        charImg: `<img src="https://static.wikia.nocookie.net/brawlstars/images/3/36/Nita_Skin-Default.png/revision/latest?cb=20231213021935" alt="">`,
+        charBgColor: "#DC143C", // crimson
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Bull",
+        charDesc: "A tough brawler with a powerful shotgun",
+        charImg: `<img src="https://static.wikia.nocookie.net/brawlstars/images/b/b7/Bull_Skin-Default.png/revision/latest?cb=20230907104019" alt="">`,
+        charBgColor: "#000000", // black
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Jessie",
+        charDesc: "A brawler who can deploy a turret to assist in attacks",
+        charImg: `<img src="https://static.wikia.nocookie.net/brawlstars/images/1/15/Jessie_Skin-Default.png/revision/latest?cb=20220421111834" alt="">`,
+        charBgColor: "#FFD700", // gold
+        charTextColor: "#000000", // black
+        charId: "charId",
+      },
+      {
+        charName: "Dynamike",
+        charDesc: "A dynamite-throwing brawler with explosive attacks",
+        charImg: `<img src="https://static.wikia.nocookie.net/brawlstars/images/6/6e/Dynamike_Skin-Default.png/revision/latest?cb=20201118121329" alt="">`,
+        charBgColor: "#808080", // gray
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Bo",
+        charDesc: "A brawler who uses explosive arrows and traps",
+        charImg: `<img src="https://static.wikia.nocookie.net/brawlstars/images/e/ee/Bo_Skin-Default.png/revision/latest?cb=20220921033755" alt="">`,
+        charBgColor: "#FF6347", // tomato
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+    ],
+  },
+  {
+    gameName: "Boom Beach",
+    gameImage: `<img src="https://mcdn.wallpapersafari.com/medium/72/71/mcjgo8.jpg" >`,
+    gameId: "gameId",
+    characters: [
+      {
+        charName: "Rifleman",
+        charDesc: "A basic infantry unit with reliable firepower",
+        charImg: `<img src="https://static.wikia.nocookie.net/boombeach/images/0/09/RiflemanD.png/revision/latest/scale-to-width-down/210?cb=20150803213044" alt="">`,
+        charBgColor: "#228B22", // forest green
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Heavy",
+        charDesc: "A heavily armored unit that soaks up damage",
+        charImg: `<img src="https://static.wikia.nocookie.net/boombeach/images/1/1e/HeavyD.png/revision/latest/scale-to-width-down/250?cb=20150803213729" alt="">`,
+        charBgColor: "#000080", // navy
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Zooka",
+        charDesc: "A long-ranged unit with high explosive damage",
+        charImg: `<img src="https://static.wikia.nocookie.net/boombeach/images/a/aa/ZookaD.png/revision/latest/scale-to-width-down/180?cb=20150803213759" alt="">`,
+        charBgColor: "#FF4500", // orange red
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Warrior",
+        charDesc: "A fast-moving melee unit with high damage output",
+        charImg: `<img src="https://static.wikia.nocookie.net/boombeach/images/b/b2/WarriorD.png/revision/latest/scale-to-width-down/250?cb=20150803213842" alt="">`,
+        charBgColor: "#8B4513", // saddle brown
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Tank",
+        charDesc: "A powerful armored vehicle with heavy firepower",
+        charImg: `<img src="https://static.wikia.nocookie.net/boombeach/images/6/67/Tanknew.png/revision/latest/scale-to-width-down/250?cb=20140415193510" alt="">`,
+        charBgColor: "#708090", // slate gray
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Medic",
+        charDesc: "A support unit that heals other troops in battle",
+        charImg: `<img src="https://static.wikia.nocookie.net/boombeach/images/4/4b/Medicc.png/revision/latest/scale-to-width-down/250?cb=20240227172705" alt="">`,
+        charBgColor: "#32CD32", // lime green
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Grenadier",
+        charDesc: "A long-ranged unit that throws grenades",
+        charImg: `<img src="https://static.wikia.nocookie.net/boombeach/images/c/c5/GrenadierB.png/revision/latest/scale-to-width-down/250?cb=20150803213922" alt="">`,
+        charBgColor: "#A52A2A", // brown
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+    ],
+  },
+  {
+    gameName: "Hay Day",
+    gameImage: `<img src="https://wallpapers.com/images/featured-full/hay-day-hbh9jhlj0c9p87kc.jpg" >`,
+    gameId: "gameId",
+    characters: [
+      {
+        charName: "Chicken",
+        charDesc: "A clucking poultry that provides eggs",
+        charImg: `<img src="https://static.wikia.nocookie.net/hayday/images/6/69/Chicken_Walking.png/revision/latest?cb=20191025192307" alt="">`,
+        charBgColor: "#FFD700", // gold
+        charTextColor: "#000000", // black
+        charId: "charId",
+      },
+      {
+        charName: "Cow",
+        charDesc: "A milk-producing bovine",
+        charImg: `<img src="https://static.wikia.nocookie.net/hayday/images/8/84/Cow.png/revision/latest?cb=20191025194659" alt="">`,
+        charBgColor: "#A52A2A", // brown
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Pig",
+        charDesc: "A plump farm animal that provides bacon",
+        charImg: `<img src="https://static.wikia.nocookie.net/hayday/images/3/30/Pig.png/revision/latest?cb=20191025201248" alt="">`,
+        charBgColor: "#FFC0CB", // pink
+        charTextColor: "#000000", // black
+        charId: "charId",
+      },
+      {
+        charName: "Sheep",
+        charDesc: "A woolly animal that provides wool",
+        charImg: `<img src="https://static.wikia.nocookie.net/hayday/images/c/cc/Sheep.png/revision/latest?cb=20191025203649" alt="">`,
+        charBgColor: "#FFFFFF", // white
+        charTextColor: "#000000", // black
+        charId: "charId",
+      },
+      {
+        charName: "Goat",
+        charDesc: "A hardy animal that produces goat milk",
+        charImg: `<img src="https://static.wikia.nocookie.net/hayday/images/5/52/Goat.png/revision/latest?cb=20191025202216" alt="">`,
+        charBgColor: "#8B4513", // saddle brown
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+      {
+        charName: "Horse",
+        charDesc: "A strong animal used for various farm activities",
+        charImg: `<img src=".https://static.wikia.nocookie.net/hayday/images/6/66/Bay_Horse.png/revision/latest?cb=20150713093010" alt="">`,
+        charBgColor: "#8A2BE2", // blue violet
+        charTextColor: "#FFFFFF", // white
+        charId: "charId",
+      },
+    ],
+  },
+];
